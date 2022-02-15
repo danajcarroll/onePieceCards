5b4162e80cff260c246bd783b49a71af6ac461f1
Dana <danacarroll2000@gmail.com>
2022-02-15 09:28:46 -0500

    Fixed time skip buttons

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ let charCard = document.getElementsByClassName('characterCards');
 let mainColor = document.getElementsByClassName('mainColor');
 let mainFontColor = document.getElementsByClassName('mainColorFont');
 let subColor = document.getElementsByClassName('subColor');
-let preButton = document.getElementsByClassName('preButton');
-let postButton = document.getElementsByClassName('postButton');
+let preButton = document.getElementById('preButton');
+let postButton = document.getElementById('postButton');
 let charPicture = document.getElementsByClassName('characterPic');
 let preSkip = document.getElementsByClassName('preSkip');
 let postSkip = document.getElementsByClassName('postSkip');
+let activeClass = document.getElementsByClassName('active');
 
 let bigName = document.getElementById('name');
 let fullName = document.getElementById('fullName');
@@ -18,25 +19,36 @@ let originPlace = document.getElementById('origin');
 let ability = document.getElementById('ability');
 let bounty = document.getElementById('bounty');
 
+
+// ******************** SETTING THINGS ********************
 // Changing character circle colours!
 for (let i = 0; i < charCircle.length; i++) {
     let circleID = charCircle[i].id;
     charCircle[i].style.backgroundColor = `var(--${circleID}Main)`;
 };
+// Setting up active image button
+preButton.classList.add('active');
+for (let i = 0; i < activeClass.length; i++) {
+    activeClass[i].style.backgroundColor = `var(--luffySub)`;
+}
 
+let circleID = 'luffy';
+console.log(circleID);
 
 // Character Button Click
 for (let i = 0; i < charCircle.length; i++) {
     charCircle[i].addEventListener('click', function() {
-        let circleID = charCircle[i].id;
+        circleID = charCircle[i].id;
         console.log(circleID);
         
+        // Changing text for each character
         bigName.innerHTML = document.getElementById(`${circleID}Name`).innerHTML;
         fullName.innerHTML = document.getElementById(`${circleID}FullName`).innerHTML;
         occupation.innerHTML = document.getElementById(`${circleID}Occupation`).innerHTML;
         originPlace.innerHTML = document.getElementById(`${circleID}Origin`).innerHTML;
         ability.innerHTML = document.getElementById(`${circleID}Ability`).innerHTML;
         bounty.innerHTML = document.getElementById(`${circleID}Bounty`).innerHTML;
+
         // Changing color theme
         for (let i = 0; i < mainColor.length; i++) {
             mainColor[i].style.backgroundColor = `var(--${circleID}Main)`;
@@ -47,42 +59,33 @@ for (let i = 0; i < charCircle.length; i++) {
         for (let i = 0; i < subColor.length; i++) {
             subColor[i].style.backgroundColor = `var(--${circleID}Sub)`;
         }
-    })
-}
-
-
-for (let i = 0; i < preButton.length; i++) {
-    preButton[i].addEventListener('click', function() {
-        for (let i = 0; i < preSkip.length; i++) {
-            preSkip[i].classList.add('activeImage');
-            preSkip[i].classList.remove('hiddenImage');
-        }
-        for (let i = 0; i < preSkip.length; i++) {
-            postSkip[i].classList.add('hiddenImage');
-        }
-        // Changing button background
-        if (!preButton[i].classList.contains('subColor')) {
-            preButton[i].classList.add('subColor');
-            postButton[i].classList.remove('subColor');
+        
+        preButton.classList.add('active');
+        preButton.style.backgroundColor =  `var(--${circleID}Sub)`;
+        postButton.classList.remove('active');
+        postButton.style.backgroundColor =  `transparent`;  
+        // This changes the background colour of the ELEMENT ITSELF. IT DOES NOT CHANGE THE CLASS' DESCRIPTION
+        for (let i = 0; i < activeClass.length; i++) {
+            activeClass[i].style.backgroundColor = `var(--${circleID}Sub)`;
         }
     })
 }
+preButton.addEventListener('click', function() {
+    if (!preButton.classList.contains('active')){
+        preButton.classList.add('active');
+        preButton.style.backgroundColor =  `var(--${circleID}Sub)`;
 
+        postButton.classList.remove('active');      
+        postButton.style.backgroundColor =  `transparent`;  
+    }
+})
+postButton.addEventListener('click', function() {
+    if (!postButton.classList.contains('active')){
+        postButton.classList.add('active');
+        postButton.style.backgroundColor =  `var(--${circleID}Sub)`;
 
-for (let i = 0; i < postButton.length; i++) {
-    postButton[i].addEventListener('click', function() {
-        for (let i = 0; i < postSkip.length; i++) {
-            postSkip[i].classList.add('activeImage');
-            postSkip[i].classList.remove('hiddenImage');
-        }
+        preButton.classList.remove('active');
+        preButton.style.backgroundColor =  `transparent`;  
+    }
+})
 
-        for (let i = 0; i < preSkip.length; i++) {
-            preSkip[i].classList.add('hiddenImage');
-        }
-
-        if (!postButton[i].classList.contains('subColor')) {
-            postButton[i].classList.add('subColor');
-            preButton[i].classList.remove('subColor');
-        }
-    })
-}
