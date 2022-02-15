42a51ef1eeae0686bff8ef771c969df3e93f6439
Dana <danacarroll2000@gmail.com>
2022-02-15 09:47:04 -0500

    Image sources change upon button click

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// Card Elements
 let charCircle = document.getElementsByClassName('characterCircles');
 let charCard = document.getElementsByClassName('characterCards');
 let mainColor = document.getElementsByClassName('mainColor');
@@ -8,16 +9,19 @@ let subColor = document.getElementsByClassName('subColor');
 let preButton = document.getElementById('preButton');
 let postButton = document.getElementById('postButton');
 let charPicture = document.getElementsByClassName('characterPic');
-let preSkip = document.getElementsByClassName('preSkip');
-let postSkip = document.getElementsByClassName('postSkip');
 let activeClass = document.getElementsByClassName('active');
 
+// Character Info
 let bigName = document.getElementById('name');
 let fullName = document.getElementById('fullName');
 let occupation = document.getElementById('occupation');
 let originPlace = document.getElementById('origin');
 let ability = document.getElementById('ability');
 let bounty = document.getElementById('bounty');
+
+// Image Sources
+let preSkipImage = document.getElementById('preSkipImage');
+let postSkipImage = document.getElementById('postSkipImage');
 
 
 // ******************** SETTING THINGS ********************
@@ -31,9 +35,9 @@ preButton.classList.add('active');
 for (let i = 0; i < activeClass.length; i++) {
     activeClass[i].style.backgroundColor = `var(--luffySub)`;
 }
-
 let circleID = 'luffy';
 console.log(circleID);
+
 
 // Character Button Click
 for (let i = 0; i < charCircle.length; i++) {
@@ -59,7 +63,14 @@ for (let i = 0; i < charCircle.length; i++) {
         for (let i = 0; i < subColor.length; i++) {
             subColor[i].style.backgroundColor = `var(--${circleID}Sub)`;
         }
+        // Image Source Change
+        preSkipImage.src = `../images/${circleID}-pre.png`;
+        postSkipImage.src = `../images/${circleID}-post.png`;
+        preSkipImage.classList.remove('hiddenImage');
+        postSkipImage.classList.add('hiddenImage');
+
         
+        // Image Button Color Change
         preButton.classList.add('active');
         preButton.style.backgroundColor =  `var(--${circleID}Sub)`;
         postButton.classList.remove('active');
@@ -74,18 +85,22 @@ preButton.addEventListener('click', function() {
     if (!preButton.classList.contains('active')){
         preButton.classList.add('active');
         preButton.style.backgroundColor =  `var(--${circleID}Sub)`;
+        preSkipImage.classList.remove('hiddenImage');
 
         postButton.classList.remove('active');      
-        postButton.style.backgroundColor =  `transparent`;  
+        postButton.style.backgroundColor =  `transparent`;
+        postSkipImage.classList.add('hiddenImage');
     }
 })
 postButton.addEventListener('click', function() {
     if (!postButton.classList.contains('active')){
         postButton.classList.add('active');
         postButton.style.backgroundColor =  `var(--${circleID}Sub)`;
+        postSkipImage.classList.remove('hiddenImage');
 
         preButton.classList.remove('active');
-        preButton.style.backgroundColor =  `transparent`;  
+        preButton.style.backgroundColor =  `transparent`;
+        preSkipImage.classList.add('hiddenImage');
     }
 })
 
